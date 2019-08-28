434f99e3a7db669655ef77b00e419d38c2d6c496
JuiChien-Lin <nturay0321@gmail.com>
2019-08-27 23:14:12 -0400

    Initial Commit

---
+++ src/main/java/com/todoconsole/TodoConsoleWS/TodoConsoleWsApplication.java
@@ -0,0 +1,13 @@
+package com.todoconsole.TodoConsoleWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoConsoleWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoConsoleWsApplication.class, args);
+	}
+
+}
